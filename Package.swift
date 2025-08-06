f091d8367f8f0b1d7fcd268dc94afe1540b8e991
GitHub Action <action@github.com>
2025-08-06 12:02:05 +0000

    Update XCFramework to version 0.1.20

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PortfolioTrackerSDK",
-            url: "https://github.com/dezerv/portfolio-sdk-ios/releases/download/v0.1.9/PortfolioTrackerSDK.xcframework.zip",
-            checksum: "7f4d81b6fbdd462502eebecfa1670b94fe0710726bda94bd4962659194c072c4"
+            url: "https://github.com/dezerv/portfolio-sdk-ios/releases/download/v0.1.20/PortfolioTrackerSDK.xcframework.zip",
+            checksum: "8cc2fb23642f10b708cc68a6d2809bb414585dc7c30e1e58a97e55b924fb6678"
         )
     ]
 )
