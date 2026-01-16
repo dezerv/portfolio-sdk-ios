0689bdbfa493e4e46175356851d1953c02882fd9
GitHub Action <action@github.com>
2026-01-16 09:01:10 +0000

    Update XCFramework to version 0.7.3

--- Package.swift
+++ Package.swift
@@ -12,21 +12,21 @@ let package = Package(
             targets: ["PortfolioTrackerSDKWrapper"]
         ),
     ],
-    # dependencies: [
-    #     .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "8.43.0")
-    # ],
+    dependencies: [
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "8.43.0")
+    ],
     targets: [
         .target(
             name: "PortfolioTrackerSDKWrapper",
             dependencies: [
                 "PortfolioTrackerSDK",
-                .product(name: "Sentry", package: "sentry-cocoa")
+                .product(name: "Sentry-Dynamic", package: "sentry-cocoa")
             ]
         ),
         .binaryTarget(
             name: "PortfolioTrackerSDK",
-            url: "https://github.com/dezerv/portfolio-sdk-ios/releases/download/v0.7.2/PortfolioTrackerSDK.xcframework.zip",
-            checksum: "44c44d5aff363d2b3926439fa6de8fe45e26308e2b00e2b6f58825cf0b57d1d1"
+            url: "https://github.com/dezerv/portfolio-sdk-ios/releases/download/v0.7.3/PortfolioTrackerSDK.xcframework.zip",
+            checksum: "6994a9955cbd6eb1fbd05d1a9d5f72bd240b19ab61d105809a358b0e2084e146"
         )
     ]
 )
